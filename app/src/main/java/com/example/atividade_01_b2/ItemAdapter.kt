95c42c70386d39686d4de14fceef371d7f5385b3
Anderson-Klein <andersonklein07@gmail.com>
2024-10-20 18:23:33 -0300

    Projeto Corigido

--- app/src/main/java/com/example/atividade_01_b2/ItemAdapter.kt
+++ app/src/main/java/com/example/atividade_01_b2/ItemAdapter.kt
@@ -1,25 +1,26 @@
 package com.example.atividade_01_b2
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.recylcerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import android.widget.TextView
 
 class ItemAdapter(private val itemList: MutableList<String>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     // Classe interna ViewHolder
-    class ViewHolder(itemView: ItemView) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextVew = itemView.findViewById(android.R.id.text1)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(android.R.id.text1)
     }
 
     // Infla o layout do item da lista
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHoolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
-        return ViewHoolder(view) // erro proposital no nome da classe retornada
+        return ViewHolder(view) // erro proposital no nome da classe retornada
     }
 
     // Liga os dados aos componentes do item
-    override fun onBindViewHolder(holder: ViewHoolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = itemList[position]
         holder.textView.text = item
     }
--- app/src/main/java/com/example/atividade_01_b2/MainActivity.kt
+++ app/src/main/java/com/example/atividade_01_b2/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.atividade_01_b2
 
 import android.os.Bundle
-import android.widget.Buttonn
-import android.widget.Editext
-import androidx.appcombat.app.AppCombatActivity
-import androidx.recycletview.widget.LinearLayoutManager
-import androidx.recycletview.widget.RecyclerView
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCombatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ItemAdapter
-    private lateinit var inputText: Edittext
-    private lateinit var addButton: Buttton
+    private lateinit var inputText: EditText
+    private lateinit var addButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
